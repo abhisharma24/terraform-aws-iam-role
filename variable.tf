0a8067459035c3234409bb2d2629527ac70807a6
Abhay sharma <theabhaysharma24@gmail.com>
2021-11-18 13:50:32 +0530

    my first terraform module

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  type = string
+  default = ""
+  description = " rahul "
+}
+variable "assume_role_policy" {
+  type = string
+  default = false
+  description = "kaml"
+}
+variable "tegs" {
+  type = map
+  default = {}
+  description = "mamraj"
+}
+
